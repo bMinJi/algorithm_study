0cd12e47eb4f6c6bc55e0f97bd501f88425b67ed
BangMinJi <balswl0496@naver.com>
2023-03-28 12:37:32 +0900

    this is coding test_4.1

--- implementation/this is coding test_4-1.py
+++ implementation/this is coding test_4-1.py
@@ -1,23 +1,28 @@
 # 상하좌우
 
-n = int(input())
-moves = input().split()
-x, y = 1, 1
+n = int(input())   # 공간의 크기
+moves = input().split()   # 이동 계획
+x, y = 1, 1   # 시작 좌표
 
-for move in moves :
-    if move == 'L' :
-        if y > 1 :
+for move in moves:
+    if move == 'L':   # 왼쪽으로 이동
+        if y > 1:
             y = y - 1
-    elif move == 'R' :
-        if y < n :
+    elif move == 'R':   # 오른쪽으로 이동
+        if y < n:
             y = y + 1
-    elif move == 'U' :
-        if x > 1 :
-            x = x - 1
-    elif move == 'D' :
-        if x < n :
-            x = x + 1        
+    elif move == 'U':   # 위로 이동
+        if x > 1:
+            x = x -1
+    elif move == 'D':   # 아래로 이동
+        if x < n:
+            x = x + 1
 
-print(x, y) 
+print(x, y)
+
+
         
+
+
+
 
